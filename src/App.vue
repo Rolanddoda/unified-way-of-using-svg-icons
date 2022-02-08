448da3b5dbae0cd884afcf8be9e563fb964e468f
Roland Doda <rolanddoda2014@gmail.com>
2022-02-08 14:19:11 +0100

    Init app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Unified way of using SVG Icons</h1>
+  </header>
+
+  <main>
+    <button>I am a button</button>
+  </main>
+</template>
+
+<style>
+@import "./css/base.css";
+
+#app {
+  display: grid;
+  place-content: center;
+  place-items: center;
+  gap: 1rem;
+}
+
+h1 {
+  font-size: 3rem;
+  font-weight: bold;
+  background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-align: center;
+}
+</style>
