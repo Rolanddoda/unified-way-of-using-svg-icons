07454f31d43574643f3f2474722b3f740ac7776c
Roland Doda <rolanddoda2014@gmail.com>
2022-02-08 14:22:29 +0100

    Add svg icon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,100 @@
+<script>
+import { defineAsyncComponent } from "vue";
+
+const sizes = {
+  sm: {
+    size: 16,
+    strokeWidth: 1,
+  },
+  md: {
+    size: 24,
+    strokeWidth: 1,
+  },
+  lg: {
+    size: 32,
+    strokeWidth: 2,
+  },
+  xl: {
+    size: 48,
+    strokeWidth: 2.5,
+  },
+};
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    color: {
+      type: String,
+      default: "var:slate",
+    },
+
+    size: {
+      type: String,
+      default: "md",
+      validator: (val) => Object.keys(sizes).includes(val),
+    },
+
+    hoverColor: [Boolean, String],
+  },
+
+  computed: {
+    dynamicComponent() {
+      return defineAsyncComponent(() =>
+        import(`src/icons/${this.name.toLowerCase()}.vue`)
+      );
+    },
+
+    colorBind() {
+      return this.getVarOrColorValue(this.color);
+    },
+
+    hoverColorBind() {
+      if (!this.hoverColor) return;
+
+      if (this.hoverColor === true) return "white";
+      return this.getVarOrColorValue(this.hoverColor);
+    },
+
+    svgSize() {
+      return sizes[this.size].size;
+    },
+
+    strokeWidth() {
+      return sizes[this.size].strokeWidth;
+    },
+  },
+
+  methods: {
+    getVarOrColorValue(str) {
+      return str.startsWith("var:")
+        ? str.replace(/^var:/, "var(--q-") + ")"
+        : str;
+    },
+  },
+};
+</script>
+
+<template>
+  <component
+    :is="dynamicComponent"
+    class="svg-icon"
+    :width="svgSize"
+    :height="svgSize"
+    :stroke-width="strokeWidth"
+    :class="{ 'cursor-pointer add-hover': !!hoverColor }"
+  />
+</template>
+
+<style lang="scss" scoped>
+.svg-icon {
+  color: v-bind(colorBind);
+
+  &.add-hover:hover {
+    color: v-bind(hoverColorBind);
+  }
+}
+</style>
