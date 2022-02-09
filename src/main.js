5cd4acd9af60d5f775a0f9972cc44ad3f9e8de53
Roland Doda <rolanddoda2014@gmail.com>
2022-02-09 11:40:38 +0100

    Add Quasar

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { Quasar } from "quasar";
 import SvgIcon from "@/components/SvgIcon.vue";
+
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css";
+// Import Quasar css
+import "quasar/src/css/index.sass";
 
 const app = createApp(App);
 
 app.component("svg-icon", SvgIcon);
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
 
 app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({ template: { transformAssetUrls } }), quasar()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
