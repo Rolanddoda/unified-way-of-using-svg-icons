4837d42355d45061b9adc8826298230cc3976956
Roland Doda <rolanddoda2014@gmail.com>
2022-02-14 08:18:02 +0100

    2 ways to register global components: glob and globEager

--- src/plugins/global-components.js
+++ src/plugins/global-components.js
@@ -1,3 +1,20 @@
+/* With import.meta.glob */
+// import { defineAsyncComponent } from 'vue'
+// import { app } from './main-app'
+//
+// const globalComponentsPaths = import.meta.glob('/src/components/global/**/*.vue')
+//
+// Object.entries(globalComponentsPaths).forEach(([path, module]) => {
+//   // "./components/SvgIcon.vue" -> "SvgIcon"
+//   const componentName = path
+//     .split('/')
+//     .pop()
+//     .replace(/\.vue$/, '')
+//
+//   app.component(componentName, defineAsyncComponent(module))
+// })
+
+/* With import.meta.globEager */
 import { app } from './main-app'
 
 const globalComponentsPaths = import.meta.globEager('/src/components/global/**/*.vue')
